c08d6f9d97ccec9cabb3750efee61a0bc6735534
duyet.vo <duyet.vo@sutrixsolutions.com>
2018-03-22 17:45:12 +0700

    init git

---
+++ src/app/hero-search/hero-search.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { Subject }    from 'rxjs/Subject';
+import { of }         from 'rxjs/observable/of';
+
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+
+import { Hero } from '../entities/hero';
+import { HeroService } from '../services/hero.service';
+
+@Component({
+  selector: 'app-hero-search',
+  templateUrl: './hero-search.component.html',
+  styleUrls: ['./hero-search.component.css']
+})
+export class HeroSearchComponent implements OnInit {
+
+  private searchTerms = new Subject<string>();
+
+  heroes$: Observable<Hero[]>;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.heroService.searchHeroes(term))
+    );
+  }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+}
