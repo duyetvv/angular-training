405fea8fe079c01cacc6a6309d97ac8dcb39180b
duyet.vo <duyet.vo@sutrixsolutions.com>
2018-05-17 18:02:16 +0700

    update

--- conduit/src/app/components/article/article.component.ts
+++ conduit/src/app/components/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Article } from '../../models';
 
 @Component({
   selector: 'app-article',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
 
+  @Input() article: Article;
+
   constructor() { }
 
   ngOnInit() {
+    debugger
   }
 
 }
